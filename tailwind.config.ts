b69215904dbfdf0d547086addfb5d35be1167688
KoksTheSupa <kogome009@yandex.ru>
2023-04-22 00:45:23 +0500

    feat: start proj

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from 'tailwindcss'
+
+export default <Partial<Config>>{
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {},
+    },
+  },
+}
